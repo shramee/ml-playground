8441c76d53a079332bfe7b9d1c6911f793d8442a
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-05-01 02:22:10 +0530

    Template for experiments

---
+++ public/-tpl/script.js
@@ -0,0 +1,13 @@
+/**
+ * ML5 P5 template
+ * @requires p5 https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.min.js
+ * @requires ml5 https://unpkg.com/ml5@0.12.2/dist/ml5.min.js
+ */
+
+function setup() {
+	createCanvas( window.innerWidth - 20, window.innerHeight - 20 );
+}
+
+function draw() {
+	background( 100 );
+}
