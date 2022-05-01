d032551fb2a00184aa2ac6fdbf5205e7a98f0e80
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-05-01 12:03:49 +0530

    Posenet on a video

---
+++ public/vid-posenet/script.js
@@ -0,0 +1,60 @@
+/**
+ * ML5 P5 template
+ * @requires p5 https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.min.js
+ * @requires ml5 https://unpkg.com/ml5@0.12.2/dist/ml5.min.js
+ */
+let
+	video, posenet, poses = [], skeletons = [];
+
+
+function setup() {
+	createCanvas( window.innerWidth - 20, window.innerHeight - 20 );
+	video = createVideo( 'test.mp4', videoReady );
+	video.hide();
+
+	posenet = ml5.poseNet( video, modelReady );
+	posenet.on( 'pose', gotPose );
+}
+
+function videoReady() {
+	video.playbackRate = .5;
+	video.volume( 0 );
+	video.currentTime = 50;;
+	video.loop();
+//	video.pause();
+}
+
+function modelReady() {
+	console.log( 'Ready' );
+}
+
+function gotPose( pose ) {
+	poses = pose.map( p => p.pose.keypoints );
+	skeletons = pose.map( p => p.skeleton );
+//	video.pause();
+}
+
+function draw() {
+	background( 100 );
+	stroke( 255 );
+	image( video, 0, 0 );
+	for ( let i = 0; i < poses.length; i ++ ) {
+		const keypoints = poses[i];
+		for ( let j = 0; j < keypoints.length; j ++ ) {
+			if ( keypoints[j].score > .7 ) {
+				const p = keypoints[j].position;
+				ellipse( p.x, p.y, 5, 5 );
+			}
+		}
+	}
+	for ( let i = 0; i < skeletons.length; i ++ ) {
+		const skel = skeletons[i];
+		for ( let j = 0; j < skel.length; j ++ ) {
+			console.log( skel );
+			const bone = skel[j];
+			if ( bone[0].score > .7 && bone[1].score > .7 ) {
+				line( bone[0].position.x, bone[0].position.y, bone[1].position.x, bone[1].position.y );
+			}
+		}
+	}
+}
