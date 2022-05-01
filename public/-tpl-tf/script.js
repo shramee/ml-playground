c1e03e488b6346878fda90ff9fac7302460d0b64
Shramee Srivastav <shramee.srivastav@gmail.com>
2022-05-01 12:04:14 +0530

    TF tpl

---
+++ public/-tpl-tf/script.js
@@ -0,0 +1,9 @@
+/**
+ * ML5 P5 template
+ * @requires p5 https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.0.0/p5.min.js
+ * @requires tf https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@2.0.0/dist/tf.min.js
+ */
+
+function setup() {}
+
+function draw() {}
